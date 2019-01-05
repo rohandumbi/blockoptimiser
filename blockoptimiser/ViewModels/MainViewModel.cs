689b5ba1ccc67063023464c0c02bf3650a5de44d
Rohan Ghosh <rohan.dumbi@gmail.com>
2019-01-05 17:48:01 +0530

    taking user input for data import

--- blockoptimiser/ViewModels/MainViewModel.cs
+++ blockoptimiser/ViewModels/MainViewModel.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace blockoptimiser.ViewModels
 {
     class MainViewModel : Screen
     {
         private string _projectId;
+        private String _fleetFileName;
+        public String ModelBearing { get; set; }
         public MainViewModel()
         {
             ProjectId = Context.ProjectId.ToString();
@@ -22,6 +25,40 @@ namespace blockoptimiser.ViewModels
             {
                 _projectId = value;
                 NotifyOfPropertyChange(() => ProjectId);
+            }
+        }
+
+        public String FleetFile
+        {
+            set { _fleetFileName = value; }
+        }
+
+        public void ImportData()
+        {
+            if (String.IsNullOrEmpty(_fleetFileName))
+            {
+                MessageBox.Show("Please select a file!");
+                return;
+            }
+            if (String.IsNullOrEmpty(ModelBearing))
+            {
+                MessageBox.Show("Please provide a value for model bearing!");
+                return;
+            }
+            try
+            {
+                //IEnumerable<FleetModel> Fleets = ReadCSV(_fleetFileName);
+                //_fleetDataAccess.DeleteAll();
+                //_fleetDataAccess.InsertFleets(Fleets);
+                //LoadFleetList();
+                //NotifyOfPropertyChange("Fleets");
+                MessageBox.Show("File selected: " + _fleetFileName + " and model bearing: " + ModelBearing);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                MessageBox.Show("Could not import the file. Please verify the file again.");
+                return;
             }
         }
     }
--- blockoptimiser/Views/MainView.xaml.cs
+++ blockoptimiser/Views/MainView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,12 @@ namespace blockoptimiser.Views
         public MainView()
         {
             InitializeComponent();
+        }
+        private void FleetFile_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                FleetFile.Text = openFileDialog.FileName;
         }
     }
 }
