4491aff6dfd45d62f43941b15f2dff0284a210da
Rohan Ghosh <rohan.dumbi@gmail.com>
2019-02-01 08:32:13 +0530

    template expression model mapping screen on expression selection.

---
+++ blockoptimiser/ViewModels/ExpressionModelMappingViewModel.cs
@@ -0,0 +1,19 @@
+﻿using blockoptimiser.Models;
+using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blockoptimiser.ViewModels
+{
+    public class ExpressionModelMappingViewModel: Screen
+    {
+        public String ExpressionName { get; set; }
+        public ExpressionModelMappingViewModel(Expression Expression)
+        {
+            ExpressionName = Expression.Name;
+        }
+    }
+}
