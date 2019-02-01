d2a4b5176c8f5a9fcd3f85ba3e1f40073ece851b
Rohan Ghosh <rohan.dumbi@gmail.com>
2019-02-01 15:58:46 +0530

    created express model mapping screen

--- blockoptimiser/ViewModels/ExpressionModelMappingViewModel.cs
+++ blockoptimiser/ViewModels/ExpressionModelMappingViewModel.cs
@@ -11,9 +11,11 @@ namespace blockoptimiser.ViewModels
     public class ExpressionModelMappingViewModel: Screen
     {
         public String ExpressionName { get; set; }
+        public BindableCollection<ExprModelMapping> ModelMapping { get; set; }
         public ExpressionModelMappingViewModel(Expression Expression)
         {
             ExpressionName = Expression.Name;
+            ModelMapping = new BindableCollection<ExprModelMapping>(Expression.modelMapping);
         }
     }
 }
