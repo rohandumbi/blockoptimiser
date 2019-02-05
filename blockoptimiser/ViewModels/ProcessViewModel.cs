d740454d092a09f6ad095547fecf7d4ca05d5737
Rohan Ghosh <rohan.dumbi@gmail.com>
2019-02-05 07:27:31 +0530

    Added buttons for object additio and noop handler functions.

--- blockoptimiser/ViewModels/ProcessViewModel.cs
+++ blockoptimiser/ViewModels/ProcessViewModel.cs
@@ -4,10 +4,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace blockoptimiser.ViewModels
 {
     class ProcessViewModel: Screen
     {
+        public void AddProcess()
+        {
+            MessageBox.Show("add a process.");
+        }
+
+        public void AddProduct()
+        {
+            MessageBox.Show("add a product.");
+        }
+
+        public void AddProductJoin()
+        {
+            MessageBox.Show("add a product join.");
+        }
     }
 }
