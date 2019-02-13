88c1f246c552b6ed2a39c4044d6b769126147552
Rohan Ghosh <rohan.dumbi@gmail.com>
2019-02-13 20:49:29 +0530

    Added template screens for the limit screens. Implemented tab view for displaying limit screens.

--- blockoptimiser/ViewModels/LimitsViewModel.cs
+++ blockoptimiser/ViewModels/LimitsViewModel.cs
@@ -4,15 +4,89 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace blockoptimiser.ViewModels
 {
-    public class LimitsViewModel: Screen
+    public class LimitsViewModel: Conductor<Object>
     {
-        public String ScreenDescription { get; set; }
+        public String PeriodButtonForeground { get; set; }
+        public String FinanceButtonForeground { get; set; }
+        public String ProcessButtonForeground { get; set; }
+        public String GradeButtonForeground { get; set; }
+        public Boolean IsPeriodSelected { get; set; }
+
         public LimitsViewModel()
         {
-            ScreenDescription = "This is Limits View.";
+            SetDisabledButtonForegrounds();
+            PeriodButtonForeground = "#FF189AD3";
+            IsPeriodSelected = false;
+            ActivateItem(new PeriodViewModel());
+        }
+
+        public void SetDefaultButtonForegrounds()
+        {
+            PeriodButtonForeground = "#FF0E1A1F";
+            FinanceButtonForeground = "#FF0E1A1F";
+            ProcessButtonForeground = "#FF0E1A1F";
+            GradeButtonForeground = "#FF0E1A1F";
+        }
+
+        public void SetDisabledButtonForegrounds()
+        {
+            FinanceButtonForeground = "#D3D3D3";
+            ProcessButtonForeground = "#D3D3D3";
+            GradeButtonForeground = "#D3D3D3";
+        }
+
+        private void NotifyButtonforegroundChanges()
+        {
+            NotifyOfPropertyChange(() => PeriodButtonForeground);
+            NotifyOfPropertyChange(() => FinanceButtonForeground);
+            NotifyOfPropertyChange(() => ProcessButtonForeground);
+            NotifyOfPropertyChange(() => GradeButtonForeground);
+        }
+
+        public void ClickTab(object sender)
+        {
+            var selectedButton = sender as Button;
+            SetDefaultButtonForegrounds();
+            NotifyButtonforegroundChanges();
+            //IsPeriodSelected = true;
+            //NotifyOfPropertyChange(() => IsPeriodSelected);
+            if (selectedButton != null)
+            {
+                String keyword = selectedButton.Content.ToString();
+                switch (keyword)
+                {
+                    case "Period":
+                        PeriodButtonForeground = "#FF189AD3";
+                        NotifyOfPropertyChange(() => PeriodButtonForeground);
+                        ActivateItem(new PeriodViewModel());
+                        IsPeriodSelected = true;
+                        NotifyOfPropertyChange(() => IsPeriodSelected);
+                        break;
+                    case "Finance":
+                        FinanceButtonForeground = "#FF189AD3";
+                        NotifyOfPropertyChange(() => FinanceButtonForeground);
+                        ActivateItem(new FinanceLimitViewModel());
+                        break;
+                    case "Process":
+                        ProcessButtonForeground = "#FF189AD3";
+                        NotifyOfPropertyChange(() => ProcessButtonForeground);
+                        ActivateItem(new ProcessLimitViewModel());
+                        break;
+                    case "Grade":
+                        GradeButtonForeground = "#FF189AD3";
+                        NotifyOfPropertyChange(() => GradeButtonForeground);
+                        ActivateItem(new GradeLimitViewModel());
+                        break;
+                    default:
+                        PeriodButtonForeground = "#FF189AD3";
+                        ActivateItem(new PeriodViewModel());
+                        break;
+                }
+            }
         }
     }
 }
