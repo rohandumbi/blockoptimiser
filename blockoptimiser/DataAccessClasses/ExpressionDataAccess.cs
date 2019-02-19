9dbaac78b2805ad390df20ca9714f5e27d203457
Arpan Bandyopadhyay <arpanbanerjeeju@gmail.com>
2019-02-19 08:39:12 +0530

    Fixing expression

--- blockoptimiser/DataAccessClasses/ExpressionDataAccess.cs
+++ blockoptimiser/DataAccessClasses/ExpressionDataAccess.cs
@@ -26,6 +26,19 @@ namespace blockoptimiser.DataAccessClasses
             }
         }
 
+        public Expression Get(int Id)
+        {
+            using (IDbConnection connection = getConnection())
+            {
+                var expression = connection.QuerySingle<Expression>($"select * from expression where Id = { Id } ");
+                expression.modelMapping
+                        = connection.Query<ExprModelMapping>($"select *, b.Name from ExprModelMapping a, Model b " +
+                        $"where b.Id = a.ModelId and ExprId = { Id }").ToList();
+                
+                return expression;
+            }
+        }
+
         public void Insert(Expression newExpression)
         {
             using (IDbConnection connection = getConnection())
@@ -40,6 +53,7 @@ namespace blockoptimiser.DataAccessClasses
                     newExpression.ProjectId,
                     newExpression.Name
                 });
+                
 
                 foreach (ExprModelMapping ModelMapping in newExpression.modelMapping)
                 {
@@ -50,6 +64,26 @@ namespace blockoptimiser.DataAccessClasses
                         ModelMapping.ModelId,
                         ModelMapping.ExprString
                     });
+
+                    // Update computed data as well. this is not a good place but I need to find a better place
+                    try
+                    {
+                        if (String.IsNullOrEmpty(ModelMapping.ExprString))
+                        {
+                            ModelMapping.ExprString = "0";
+                        }
+                        connection.Execute($"ALTER TABLE BOData_Computed_{ newExpression.ProjectId }_{ ModelMapping.ModelId }" +
+                       $" ADD { newExpression.Name } DECIMAL(18,10) ");
+                        String updateQuery = $" update BOData_Computed_{ newExpression.ProjectId }_{ ModelMapping.ModelId }" +
+                             $" set { newExpression.Name } = ( select ISNULL( {ModelMapping.ExprString} , 0) from BOData_{ newExpression.ProjectId }_{ ModelMapping.ModelId } a " +
+                             $"where a.Id = BOData_Computed_{ newExpression.ProjectId }_{ ModelMapping.ModelId }.Id )";
+                        connection.Execute(updateQuery);
+                    }
+                    catch(Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                   
                 }
             }
         }
@@ -67,6 +101,26 @@ namespace blockoptimiser.DataAccessClasses
                     newMapping.ModelId,
                     newMapping.ExprString
                 });
+
+                Expression expression = this.Get(newMapping.ExprId);
+                try
+                {
+                    if(String.IsNullOrEmpty(newMapping.ExprString))
+                    {
+                        newMapping.ExprString = "0";
+                    }
+                    connection.Execute($"ALTER TABLE BOData_Computed_{ expression.ProjectId }_{ newMapping.ModelId }" +
+                   $" ADD { expression.Name } DECIMAL(18,10) ");
+                    String updateQuery = $" update BOData_Computed_{ expression.ProjectId }_{ newMapping.ModelId }" +
+                             $" set { expression.Name } = ( select ISNULL( {newMapping.ExprString} , 0) from BOData_{ expression.ProjectId }_{ newMapping.ModelId } a " +
+                             $"where a.Id = BOData_Computed_{ expression.ProjectId }_{ newMapping.ModelId }.Id )";
+                    connection.Execute(updateQuery);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                
             }
         }
 
@@ -81,6 +135,22 @@ namespace blockoptimiser.DataAccessClasses
                     updatedMapping.ExprId,
                     updatedMapping.ModelId
                 });
+                try
+                {
+                    if (String.IsNullOrEmpty(updatedMapping.ExprString))
+                    {
+                        updatedMapping.ExprString = "0";
+                    }
+                    Expression expression = this.Get(updatedMapping.ExprId);
+                    String sql = $" update BOData_Computed_{ expression.ProjectId }_{ updatedMapping.ModelId }" +
+                        $" set { expression.Name } = ( select ISNULL( {updatedMapping.ExprString} , 0) from BOData_{ expression.ProjectId }_{ updatedMapping.ModelId } a " +
+                        $"where a.Id = BOData_Computed_{ expression.ProjectId }_{ updatedMapping.ModelId }.Id )";
+                    connection.Execute(sql);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
             }
         }
 
@@ -100,6 +170,24 @@ namespace blockoptimiser.DataAccessClasses
                         ModelMapping.ModelId,
                         ModelMapping.ExprString
                     });
+                    try
+                    {
+                        if (String.IsNullOrEmpty(ModelMapping.ExprString))
+                        {
+                            ModelMapping.ExprString = "0";
+                        }
+                        connection.Execute($"ALTER TABLE BOData_Computed_{ expression.ProjectId }_{ ModelMapping.ModelId }" +
+                       $" ADD { expression.Name } DECIMAL(18,10) ");
+                        
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                    String updateQuery = $" update BOData_Computed_{ expression.ProjectId }_{ ModelMapping.ModelId }" +
+                        $" set { expression.Name } = ( select ISNULL( {ModelMapping.ExprString} , 0) from BOData_{ expression.ProjectId }_{ ModelMapping.ModelId } a " +
+                        $"where a.Id = BOData_Computed_{ expression.ProjectId }_{ ModelMapping.ModelId }.Id )";
+                    connection.Execute(updateQuery);
                 }
             }
         }
