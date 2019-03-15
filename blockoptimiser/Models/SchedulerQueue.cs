99046671a7ed94cd1ba5eaf9a25e3b53b28d6c0e
Arpan Bandyopadhyay <arpanbanerjeeju@gmail.com>
2019-03-15 09:37:44 +0530

    Initial implementation of establishing communication between cplex and java code

---
+++ blockoptimiser/Models/SchedulerQueue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blockoptimiser.Models
+{
+    class SchedulerQueue
+    {
+        public int Id { get; set; }
+        public int ProjectId { get; set; }
+        public String FileName { get; set; }
+        public Boolean IsProcessed { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
