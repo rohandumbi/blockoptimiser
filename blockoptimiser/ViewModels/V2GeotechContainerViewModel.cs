fdc0ebde024ea506c263e64722814c894643bff8
Rohan Ghosh <rohan.dumbi@gmail.com>
2019-04-16 14:06:39 +0530

    Implemented tab control in geotech container view and aded v2 of new subviews as templates

--- blockoptimiser/ViewModels/V2GeotechContainerViewModel.cs
+++ blockoptimiser/ViewModels/V2GeotechContainerViewModel.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 
-namespace blockoptimiser.Views
+namespace blockoptimiser.ViewModels
 {
     public class V2GeotechContainerViewModel : Conductor<Object>
     {
