7377bf74f85e18da54633e5b3bdafc41161542d3
Rohan Ghosh <rohan.dumbi@gmail.com>
2019-04-23 16:22:42 -0400

    Partial fix expression screen

--- blockoptimiser/Views/V2ExpressionModelMappingView.xaml.cs
+++ blockoptimiser/Views/V2ExpressionModelMappingView.xaml.cs
@@ -24,7 +24,7 @@ namespace blockoptimiser.Views
         public V2ExpressionModelMappingView(blockoptimiser.Models.Expression expression)
         {
             InitializeComponent();
-            this.DataContext = new ExpressionModelMappingViewModel(expression);
+            this.DataContext = new V2ExpressionModelMappingViewModel(expression);
         }
     }
 }
